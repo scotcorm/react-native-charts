4b9713a6773f58b9b7d8ffcbc9e0ab7811b398c9
Scott Cormier <chicormier@gmail.com>
2023-12-30 15:05:49 -0600

    bar chart

--- App.js
+++ App.js
@@ -1,12 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
+// import { G, Rect, Svg, Text } from 'react-native-svg';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={{ position: 'relative', marginTop: 15 }}>
+      <Card wrapperStyle={{ marginTop: 30 }}>
+        <Card.Title>Six-Month Production Data</Card.Title>
+        <Card.Divider />
+      </Card>
+
+      <BarChart
+        data={{
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+              ],
+            },
+          ],
+        }}
+        width={Dimensions.get('window').width} // from react-native
+        height={350}
+        yAxisLabel='$'
+        yAxisSuffix='k'
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#273746',
+          backgroundGradientTo: '#1E90FF',
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: '6',
+            strokeWidth: '2',
+            stroke: '#ffa726',
+          },
+        }}
+        bezier
+        style={{
+          marginTop: 50,
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+    </ScrollView>
   );
 }
 
